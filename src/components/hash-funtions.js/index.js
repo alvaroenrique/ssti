4a8c84e6b50ba08224d57f60fadc54d1d75528eb
Alvaro Huamaní <31602481+alvaroenrique@users.noreply.github.com>
2020-02-18 20:52:35 -0500

    Actualizado estructura de las funciones hash

--- src/components/hash-funtions.js/index.js
+++ src/components/hash-funtions.js/index.js
@@ -3,14 +3,9 @@ import md5 from "md5"
 export const PEPPER = "SSTI-TRABAJO-GRUPAL"
 
 export const hash = (rawPassword, options = {}) => {
-  /**
-   * salt is optional, if not provided it will be set to current timestamp
-   */
+  
   const salt = options.salt ? options.salt : new Date().getTime()
 
-  /**
-   * rounds is optional, if not provided it will be set to 10
-   */
   const rounds = options.rounds ? options.rounds : 10
 
   let hashed = md5(rawPassword + PEPPER + salt)
@@ -30,6 +25,7 @@ export const compare = (rawPassword, hashedPassword) => {
     const hashedRawPassword = hash(`${rawPassword}`, { salt, rounds })
 
     return hashedPassword === hashedRawPassword
+    
   } catch (error) {
     return false
   }
